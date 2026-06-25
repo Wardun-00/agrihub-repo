89e3dc6ff5fcc033029dd34e7f55d3c16c33f322
wardun-00 <wardun.bd@gmail.com>
2026-06-25 11:42:46 +0600

    addded components and js nav and crcreat all section part

---
+++ assets/js/navbar.js
@@ -0,0 +1,24 @@
+fetch("../../components/navbar.html")
+.then(response => response.text())
+.then(data =>{
+    document.getElementById("navbar").innerHTML = data;
+
+
+   // Active Menu
+    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+    const links = document.querySelectorAll("#navbar a");
+
+    links.forEach(link => {
+      const href = link.getAttribute("href");
+
+      if (href === currentPage) {
+        link.classList.add(
+          "text-green-600",
+          "font-bold",
+          "border-b-2",
+          "border-green-600"
+        );
+      }
+    });
+  });
